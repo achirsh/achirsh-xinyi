0a1e39e0f413f2c83dac7387ac2cbb6b0644f292
geqb <geqb@shanhai.team>
2023-12-22 17:35:47 +0800

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
-import { Features } from "./components/features";
+// import { Features } from "./components/features";
 import { About } from "./components/about";
-import { Services } from "./components/services";
+// import { Services } from "./components/services";
 import { Gallery } from "./components/gallery";
 // import { Testimonials } from "./components/testimonials";
 import { Team } from "./components/Team";
